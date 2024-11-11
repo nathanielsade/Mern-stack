a6bea2d5fc585a1610e0c304c852f118daf4e3ce
Nathaniel Sade <netanelsbt@gmail.com>
2024-11-11 11:32:09 +0200

    start the environment

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+app.use((req,res,next) => {
+    console.log( req.path , req.method)
+    next()
+})
+
+app.get('/',(req,res) =>{
+    res.json({mssg: 'Welcome to my API'})
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port', process.env.PORT)
+})
