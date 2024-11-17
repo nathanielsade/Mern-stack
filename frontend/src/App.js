fe2764c27274051cbe787f4f6079e859a953c2ee
Nathaniel Sade <netanelsbt@gmail.com>
2024-11-17 19:33:05 +0200

    first api req,and add style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
-import {BrowserRouter, Routes,Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages & components
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <div className ='pages'>
+        <div className="pages">
           <Routes>
-            <Route path ='/' element ={<Home />}/>
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
           </Routes>
         </div>
       </BrowserRouter>
